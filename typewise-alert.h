dfb17bc800d7d297233284b1f987a3ccd37807ad
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-02-23 00:18:31 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#define PASSIVE_COOLING_LOWER_LIM 0
+#define PASSIVE_COOLING_UPPER_LIM 35
+#define HI_ACTIVE_COOLING_LOWER_LIM 0
+#define HI_ACTIVE_COOLING_UPPER_LIM 45
+#define MED_ACTIVE_COOLING_LOWER_LIM 0
+#define MED_ACTIVE_COOLING_UPPER_LIM 35
+
+
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
+  MED_ACTIVE_COOLING,
+  COOLING_TYPE
 } CoolingType;
 
 typedef enum {
@@ -11,9 +20,6 @@ typedef enum {
   TOO_LOW,
   TOO_HIGH
 } BreachType;
-
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 typedef enum {
   TO_CONTROLLER,
@@ -25,8 +31,20 @@ typedef struct {
   char brand[48];
 } BatteryCharacter;
 
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+struct TemperatureLimit
+{
+  int lowerLimit;
+  int upperLimit;
+};
 
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
+struct TemperatureLimit limit[COOLING_TYPE];
+
+int sendToController(BreachType breachType);
+int sendToEmail(BreachType breachType);
+
+/* structure for cooling type limits  */
+struct TemperatureLimit limit[COOLING_TYPE]={
+{PASSIVE_COOLING_LOWER_LIM,PASSIVE_COOLING_UPPER_LIM}, 
+{HI_ACTIVE_COOLING_LOWER_LIM,HI_ACTIVE_COOLING_UPPER_LIM},
+{MED_ACTIVE_COOLING_LOWER_LIM,MED_ACTIVE_COOLING_UPPER_LIM}
+};
