81646afdd85b399bb11669c8d9a5242c60b965c2
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-02-23 00:33:13 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -17,23 +17,23 @@ BreachType inferBreach(CoolingType coolingType, double temperatureInC)
 }
 
 /* Alert target selection  */
-char *BreachMessage[] = {"Hi, battery temperature is Ok\n", "Hi, the temperature is too low\n", "Hi, the temperature is too high\n"};
+const char *BreachMessage[] = {"Hi, battery temperature is Ok\n", "Hi, the temperature is too low\n", "Hi, the temperature is too high\n"};
 
 BreachType checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
 {
 BreachType breachType = inferBreach(batteryChar.coolingType, temperatureInC);
-char Target1 = (alertTarget == TO_CONTROLLER) ? sendToController(breachType) : sendToEmail(breachType);
-char Target2 = (alertTarget == TO_EMAIL) ? sendToEmail(breachType) : sendToController(breachType);
-char Target = Target1 | Target2;
+int Target1 = (alertTarget == TO_CONTROLLER) ? sendToController(breachType) : sendToEmail(breachType);
+int Target2 = (alertTarget == TO_EMAIL) ? sendToEmail(breachType) : sendToController(breachType);
+int Target = Target1 | Target2;
 return Target;
 }
 
-int sendToController(BreachType breachType) {
+void sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
 }
 
-int sendToEmail(BreachType breachType) {
+void sendToEmail(BreachType breachType) {
 const char* recipient = "a.b@c.com";
 printf("To: %s\n", recipient);
 printf("%s\n",BreachMessage[breachType]);
